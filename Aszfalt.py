3004865029bb30a3461eb0b946c5e46d9dacd9ab
Milla321 <talianljudmila@gmail.com>
2020-11-04 20:15:02 +0100

    Utaszfalt

---
+++ Aszfalt.py
@@ -0,0 +1,29 @@
+# Talián Milla
+# 2020.10.20
+# Esti szoft.
+
+szam1 = 12	# út vastagsága méterben
+szam2 = 900  		# út szélessége méterben
+szam3 = 350000    # út hosszusága méterben
+
+import math
+
+print("Talián Milla, 2020.10.20, Esti szoftver")
+
+szam1 = int(input('Szám1: '))
+szam2 = int(input('Szám2: '))
+szam3 = int(input('Szám1: '))
+
+eredmeny = (szam1 * szam2 * szam3) / 100 # aszfalt mennyiseg
+
+
+print('Eredmény "aszfalt mennyiseg": ', eredmeny , "m2")
+
+
+
+
+
+
+
+
+
